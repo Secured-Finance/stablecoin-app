d988f4f5ec15d551efc90ff0cdadd20b464736cc
Arpit Singh <arpit@secured-finance.com>
2024-08-28 23:49:18 +0530

    fix: add dummy change to test cloudflare connection [SF-1253]

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 function EntryPoint() {
     return (
-        <div className='flex h-full h-screen items-center justify-center text-white'>
-            Welcome to the Stable Coin Project!
+        <div className='flex h-screen flex-col items-center justify-center gap-2'>
+            <span className='text-16 text-white'>
+                Welcome to the Stable Coin Project!
+            </span>
+            <span className='text-10 text-teal-300'>Stay Tuned!</span>
         </div>
     );
 }
