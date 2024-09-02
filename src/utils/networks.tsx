b0b8e3b51626055968ab66106f5181467bbb4d7e
Arpit Singh <arpit@secured-finance.com>
2024-09-03 02:24:20 +0530

    fix: remove extra assets from the repo [SF-1253]

--- src/utils/networks.tsx
+++ src/utils/networks.tsx
@@ -1,18 +1,10 @@
-import Arbitrum from 'src/assets/icons/arbitrum-network.svg';
-import Avalanche from 'src/assets/icons/avalanche-network.svg';
 import Ethereum from 'src/assets/icons/ethereum-network.svg';
 import Filecoin from 'src/assets/icons/filecoin-network.svg';
-import Polygon from 'src/assets/icons/polygon-network.svg';
 import {
     Chain,
-    arbitrum,
-    arbitrumSepolia,
-    avalanche,
-    avalancheFuji,
     filecoin,
     filecoinCalibration,
     mainnet,
-    polygonZkEvm,
     sepolia,
 } from 'viem/chains';
 import { isProdEnv } from './isProdEnv';
@@ -23,11 +15,9 @@ type ChainInformation = {
 };
 
 // it is important to keep sepolia as first chain in this list
-// arbitrumSepolia used as a temporary first chain
 const testnetNetworks: Chain[] = [sepolia, filecoinCalibration];
 
 // it is important to keep mainnet as first chain in this list
-// arbitrum used as a temporary first chain
 const mainnetNetworks: Chain[] = [mainnet, filecoin];
 
 export const getSupportedNetworks = () => {
@@ -38,45 +28,15 @@ export const getSupportedNetworks = () => {
 
 export const SupportedChainsList: ChainInformation[] = [
     {
-        chain: arbitrum,
-        icon: (
-            <Arbitrum className='h-4 w-4 rounded-full tablet:h-5 tablet:w-5' />
-        ),
-    },
-    {
         chain: mainnet,
         icon: (
             <Ethereum className='h-4 w-4 rounded-full tablet:h-5 tablet:w-5' />
         ),
     },
     {
-        chain: avalanche,
-        icon: (
-            <Avalanche className='h-4 w-4 rounded-full tablet:h-5 tablet:w-5' />
-        ),
-    },
-    {
-        chain: polygonZkEvm,
-        icon: (
-            <Polygon className='h-4 w-4 rounded-full tablet:h-5 tablet:w-5' />
-        ),
-    },
-    {
-        chain: arbitrumSepolia,
-        icon: (
-            <Arbitrum className='h-4 w-4 rounded-full tablet:h-5 tablet:w-5' />
-        ),
-    },
-    {
         chain: sepolia,
         icon: (
             <Ethereum className='h-4 w-4 rounded-full tablet:h-5 tablet:w-5' />
-        ),
-    },
-    {
-        chain: avalancheFuji,
-        icon: (
-            <Avalanche className='h-4 w-4 rounded-full tablet:h-5 tablet:w-5' />
         ),
     },
     {
