751df1d5cdd5da4f8eadb6867ae8c7aeb1716755
Arpit Singh <arpit@secured-finance.com>
2024-09-12 01:31:42 +0530

    feat: add a generic table [SF-1259]

--- src/components/pages/Earn/Earn.tsx
+++ src/components/pages/Earn/Earn.tsx
@@ -1,13 +1,42 @@
-import { ManageStabilityPool } from 'src/components/organisms';
+import { ColumnDef } from '@tanstack/react-table';
 import { Page } from 'src/components/templates';
-import { CurrencySymbol } from 'src/utils';
+import { DataTable } from 'src/components/ui';
+
+export type Payment = {
+    id: string;
+    amount: number;
+    status: 'pending' | 'processing' | 'success' | 'failed';
+    email: string;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+    {
+        accessorKey: 'status',
+        header: 'Status',
+    },
+    {
+        accessorKey: 'email',
+        header: 'Email',
+    },
+    {
+        accessorKey: 'amount',
+        header: 'Amount',
+    },
+];
+
+const data: Payment[] = [
+    {
+        id: '728ed52f',
+        amount: 100,
+        status: 'pending',
+        email: 'm@example.com',
+    },
+];
 
 export const Earn = () => {
-    const currency = CurrencySymbol.sfUSD;
-
     return (
         <Page name='earn'>
-            <ManageStabilityPool currency={currency} />
+            <DataTable columns={columns} data={data} />
         </Page>
     );
 };
