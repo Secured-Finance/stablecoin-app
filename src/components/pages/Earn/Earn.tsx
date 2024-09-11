e51bf100942c91cfdb2305be48f0a614c8c21032
Arpit Singh <arpit@secured-finance.com>
2024-09-12 02:46:02 +0530

    feat: add stability pool table [SF-1259]

--- src/components/pages/Earn/Earn.tsx
+++ src/components/pages/Earn/Earn.tsx
@@ -1,42 +1,182 @@
 import { ColumnDef } from '@tanstack/react-table';
 import { Page } from 'src/components/templates';
-import { DataTable } from 'src/components/ui';
+import { Button, DataTable } from 'src/components/ui';
+import {
+    currencyMap,
+    CurrencySymbol,
+    ordinaryFormat,
+    percentFormat,
+    usdFormat,
+} from 'src/utils';
 
 export type Payment = {
     id: string;
-    amount: number;
-    status: 'pending' | 'processing' | 'success' | 'failed';
-    email: string;
+    currency: CurrencySymbol;
+    tvl: number;
+    unboostedApr: number;
+    boostedApr: number;
+    deposit: number;
+    earned: number;
 };
 
 export const columns: ColumnDef<Payment>[] = [
     {
-        accessorKey: 'status',
-        header: 'Status',
+        accessorKey: 'currency',
+        header: '',
+        cell: ({ row }) => {
+            const currency = row.original.currency;
+            const Icon = currencyMap[currency].icon;
+
+            return (
+                <div className='flex items-center gap-1.5 p-4'>
+                    <Icon
+                        className='h-8 w-8'
+                        role='img'
+                        aria-label={currency}
+                    />
+                    <span className='text-4 leading-5 text-neutral-900'>
+                        {currency}
+                    </span>
+                </div>
+            );
+        },
     },
     {
-        accessorKey: 'email',
-        header: 'Email',
+        accessorKey: 'tvl',
+        header: 'TVL',
+        cell: ({ row }) => {
+            return (
+                <div className='flex items-center px-4'>
+                    <span className='text-4 leading-5 text-neutral-900'>
+                        {usdFormat(row.original.tvl, 2, 'compact')}
+                    </span>
+                </div>
+            );
+        },
     },
     {
-        accessorKey: 'amount',
-        header: 'Amount',
+        accessorKey: 'unboostedApr',
+        header: 'Unboosted APR',
+        cell: ({ row }) => {
+            return (
+                <div className='flex items-center px-4'>
+                    <span className='text-4 leading-5 text-neutral-900'>
+                        {percentFormat(row.original.unboostedApr, 100, 2, 2)}
+                    </span>
+                </div>
+            );
+        },
+    },
+    {
+        accessorKey: 'boostedApr',
+        header: 'Boosted APR',
+        cell: ({ row }) => {
+            return (
+                <div className='flex items-center px-4'>
+                    <span className='text-4 leading-5 text-neutral-900'>
+                        {percentFormat(row.original.boostedApr, 100, 2, 2)}
+                    </span>
+                </div>
+            );
+        },
+    },
+    {
+        accessorKey: 'deposit',
+        header: 'Your Deposits',
+        cell: ({ row }) => {
+            const currency = row.original.currency;
+            const Icon = currencyMap[currency].icon;
+
+            return (
+                <div className='flex items-center gap-1 px-4'>
+                    <Icon
+                        className='h-[22px] w-[22px]'
+                        role='img'
+                        aria-label={currency}
+                    />
+                    <span className='text-4 leading-5 text-neutral-900'>
+                        {ordinaryFormat(row.original.deposit, 2, 2)}
+                    </span>
+                </div>
+            );
+        },
+    },
+    {
+        accessorKey: 'earned',
+        header: 'Earned',
+        cell: ({ row }) => {
+            const currency = row.original.currency;
+            const Icon = currencyMap[currency].icon;
+
+            return (
+                <div className='flex items-center gap-1 px-4'>
+                    <Icon
+                        className='h-[22px] w-[22px]'
+                        role='img'
+                        aria-label={currency}
+                    />
+                    <span className='text-4 leading-5 text-neutral-900'>
+                        {ordinaryFormat(row.original.earned, 2, 2)}
+                    </span>
+                </div>
+            );
+        },
+    },
+    {
+        id: 'actions',
+        header: '',
+        cell: ({ row }) => {
+            const currency = row.original.currency;
+
+            return (
+                <div className='flex w-fit items-center px-4'>
+                    <Button
+                        className='px-10 py-2'
+                        // eslint-disable-next-line no-console
+                        onClick={() => console.log('clicked', currency)}
+                    >
+                        Manage
+                    </Button>
+                </div>
+            );
+        },
     },
 ];
 
 const data: Payment[] = [
     {
         id: '728ed52f',
-        amount: 100,
-        status: 'pending',
-        email: 'm@example.com',
+        currency: CurrencySymbol.sfUSD,
+        tvl: 443430,
+        unboostedApr: 4.07,
+        boostedApr: 8.13,
+        deposit: 0.0,
+        earned: 0.0,
+    },
+    {
+        id: '728ed52',
+        currency: CurrencySymbol.sfUSD,
+        tvl: 71533,
+        unboostedApr: 10.05,
+        boostedApr: 20.9,
+        deposit: 0.0,
+        earned: 0.0,
     },
 ];
 
 export const Earn = () => {
     return (
         <Page name='earn'>
-            <DataTable columns={columns} data={data} />
+            <div className='px-36'>
+                <div className='flex h-full flex-1 flex-col rounded-xl bg-background pb-4'>
+                    <div className='flex items-center justify-between px-6 pb-2 pt-6'>
+                        <h2 className='text-4.5 font-semibold leading-[26px] text-neutral-900'>
+                            Stability Pool
+                        </h2>
+                    </div>
+                    <DataTable data={data} columns={columns} />
+                </div>
+            </div>
         </Page>
     );
 };
--- src/components/ui/DataTable.tsx
+++ src/components/ui/DataTable.tsx
@@ -23,7 +23,6 @@ import {
     TableHeader,
     TableRow,
 } from 'src/components/ui';
-import { DataTablePagination } from './DataTablePagination';
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
@@ -64,61 +63,56 @@ export function DataTable<TData, TValue>({
     });
 
     return (
-        <div className='space-y-4'>
-            <div className='rounded-md border'>
-                <Table>
-                    <TableHeader>
-                        {table.getHeaderGroups().map(headerGroup => (
-                            <TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map(header => {
-                                    return (
-                                        <TableHead key={header.id}>
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(
-                                                      header.column.columnDef
-                                                          .header,
-                                                      header.getContext()
-                                                  )}
-                                        </TableHead>
-                                    );
-                                })}
+        <div>
+            <Table>
+                <TableHeader>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map(header => {
+                                return (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                  header.column.columnDef
+                                                      .header,
+                                                  header.getContext()
+                                              )}
+                                    </TableHead>
+                                );
+                            })}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map(row => (
+                            <TableRow
+                                key={row.id}
+                                data-state={row.getIsSelected() && 'selected'}
+                            >
+                                {row.getVisibleCells().map(cell => (
+                                    <TableCell key={cell.id} className='p-0'>
+                                        {flexRender(
+                                            cell.column.columnDef.cell,
+                                            cell.getContext()
+                                        )}
+                                    </TableCell>
+                                ))}
                             </TableRow>
-                        ))}
-                    </TableHeader>
-                    <TableBody>
-                        {table.getRowModel().rows?.length ? (
-                            table.getRowModel().rows.map(row => (
-                                <TableRow
-                                    key={row.id}
-                                    data-state={
-                                        row.getIsSelected() && 'selected'
-                                    }
-                                >
-                                    {row.getVisibleCells().map(cell => (
-                                        <TableCell key={cell.id}>
-                                            {flexRender(
-                                                cell.column.columnDef.cell,
-                                                cell.getContext()
-                                            )}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            ))
-                        ) : (
-                            <TableRow>
-                                <TableCell
-                                    colSpan={columns.length}
-                                    className='h-24 text-center'
-                                >
-                                    No results.
-                                </TableCell>
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            </div>
-            <DataTablePagination table={table} />
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell
+                                colSpan={columns.length}
+                                className='h-24 text-center'
+                            >
+                                No results.
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
         </div>
     );
 }
