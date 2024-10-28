9d786375ad85e15c1f2437c354b1caa366c47574
Hadi Lee <leeshuzheng@gmail.com>
2024-10-28 17:13:41 +0800

    feat: fix icon sizing [SF-1270]

--- src/components/InfoIcon.tsx
+++ src/components/InfoIcon.tsx
@@ -5,13 +5,18 @@ import { Tooltip } from 'src/components/atoms';
 export const InfoIcon = ({
     placement = 'right',
     message,
+    Icon,
 }: {
     placement?: Parameters<typeof Tooltip>[0]['placement'];
     message: React.ReactNode;
+    Icon?: React.ElementType;
 }) => {
+    const DisplayIcon = Icon ?? Info;
     return (
         <Tooltip
-            iconElement={<Info className='h-3.5 w-3.5 text-neutral-500' />}
+            iconElement={
+                <DisplayIcon className='h-3.5 w-3.5 text-neutral-500' />
+            }
             placement={placement}
         >
             {message}
