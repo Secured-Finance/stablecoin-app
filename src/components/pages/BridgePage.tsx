a8cb20185a27921a9e29162a0ce9101e036550bb
Hadi Lee <leeshuzheng@gmail.com>
2024-12-06 12:09:26 +0800

    feat: change width of squid router [SF-1286]

--- src/components/pages/BridgePage.tsx
+++ src/components/pages/BridgePage.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useBreakpoint } from 'src/hooks';
 import { squidConfig } from 'src/utils';
 
 export const BridgePage: React.FC = () => {
+    const isMobile = useBreakpoint('laptop');
     const configQueryParam = encodeURIComponent(JSON.stringify(squidConfig));
 
     return (
         <section className='mt-5 flex justify-center overflow-hidden rounded-2xl'>
             <iframe
                 title='squid_widget'
-                width='390'
+                width={isMobile ? '390' : '430'}
                 height='684'
                 src={`https://studio.squidrouter.com/iframe?config=${configQueryParam}`}
             />
