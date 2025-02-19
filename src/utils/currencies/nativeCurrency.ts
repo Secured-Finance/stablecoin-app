3cd7fe105d0cedf831c8b16209e7485d301b8c33
akihiro tanaka <big.a.0816@gmail.com>
2025-02-19 13:51:46 +0900

    refactor: remove unused files [SF-1336]

--- src/utils/currencies/nativeCurrency.ts
+++
@@ -1,6 +0,0 @@
-import { BaseCurrency } from './baseCurrency';
-
-export abstract class NativeCurrency extends BaseCurrency {
-    public readonly isNative = true;
-    public readonly isToken = false;
-}
