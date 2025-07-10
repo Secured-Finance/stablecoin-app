9e0193dcb0d0b531eaaab7251274b1447a2de44f
vijayeth <vijay@secured-finance.com>
2025-07-10 08:44:44 +0530

    feat: trove pages [SF-1434]

---
+++ src/components/pages/TrovePage.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useSfStablecoinSelector } from 'src/hooks';
+import { useAccount } from 'wagmi';
+import { Adjusting } from '../Trove/Adjusting';
+import { Opening } from '../Trove/Opening';
+import { Trove } from '../organisms/Trove/Trove';
+
+export const TrovePage = () => {
+    const [activeTab, setActiveTab] = useState('create');
+    const { isConnected } = useAccount();
+
+    const trove = useSfStablecoinSelector(state => state.trove);
+    useEffect(() => {
+        if (trove && isConnected) {
+            setActiveTab('manage');
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <div className='min-h-[100vh] w-full'>
+            <main className='container mx-auto px-4 py-10'>
+                <h1 className='text-3xl mb-8 font-bold'>Your Trove</h1>
+
+                {activeTab === 'create' && <Opening />}
+                {activeTab === 'manage' && isConnected && (
+                    <>
+                        {trove && <Trove />}
+                        <Adjusting />
+                    </>
+                )}
+            </main>
+        </div>
+    );
+};
