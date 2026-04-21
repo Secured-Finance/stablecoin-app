e47ededb51173f6dc224fe09ca1efd00f30e975a
Arpit Singh <arpit@secured-finance.com>
2026-04-21 13:29:49 +0530

    fix: update usdfc logo [SF-1520]

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,15 @@
 import { AddressZero } from '@ethersproject/constants';
 import { SfStablecoinStoreState } from '@secured-finance/stablecoin-lib-base';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import USDFCLogoSmall from 'src/assets/img/usdfc-logo-small.svg';
+import USDFCLogo from 'src/assets/img/usdfc-logo.svg';
 import { useSfStablecoin, useSfStablecoinSelector } from 'src/hooks';
 import { navigateToTop } from 'src/utils/navigation';
 import { Container } from 'theme-ui';
 import { Nav } from './Nav';
-import { USDFCIconLarge } from './SecuredFinanceLogo';
 import { SideNav } from './SideNav';
 import { UserAccount } from './UserAccount';
-import { Link } from 'react-router-dom';
 
 const select = ({ frontend }: SfStablecoinStoreState) => ({
     frontend,
@@ -32,10 +33,8 @@ export const Header: React.FC<React.PropsWithChildren> = ({ children }) => {
                         aria-label='Home'
                         onClick={() => navigateToTop()}
                     >
-                        <USDFCIconLarge />
-                        <span className='hidden h-[31px] w-[78px] items-center font-numerical text-6 font-semibold laptop:flex'>
-                            USDFC
-                        </span>
+                        <USDFCLogoSmall className='flex h-[34px] w-[34px] laptop:hidden' />
+                        <USDFCLogo className='hidden h-[34px] w-[112px] laptop:flex' />
                     </Link>
 
                     {isFrontendRegistered && (
